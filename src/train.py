a9170b99513fbdf8a2222ba0cdd8e3ed50d08d11
masha <masha.kuznet@gmail.com>
2025-12-21 19:09:09 +0300

    Homework 4: Add reproducible DVC pipeline

--- src/train.py
+++ src/train.py
@@ -7,7 +7,7 @@ from typing import Any, Dict, Tuple
 import joblib
 import pandas as pd
 
-from src.Different_models import make_model
+from src.models.Different_models import make_model
 
 PROCESSED_TRAIN_PATH = "data/processed/train_clean.csv"
 MODEL_OUT_PATH = "models/model.pkl"
